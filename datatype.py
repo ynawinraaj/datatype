7d8edd082ed716bdb56538096ffa99fa33d56fec
ynawinraaj <gomsyuva19@gmail.com>
2024-09-16 20:48:25 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,21 @@
+name="nawinraaj"
+age=13
+is_student=True
+weight=40.8
+
+print("name:" ,name)
+print("datatype of name is" ,type(name))
+print("age:" ,age)
+print("datatype of age is" ,type(age))
+print("is_student:" ,is_student)
+print("datatype of is_student is" ,type(is_student))
+print("weight:" ,weight)
+print("datatype of weight is" ,type(weight))
+
+print("/n after typecasting")
+age=str(age)
+print("age:" ,age)
+print("datatype of age is" ,type(age))
+weight=int(weight)
+print("weight:" ,weight)
+print("datatype of weight is" ,type(weight))
